c68d60ae4b50a41306e66f52d4966b8e62aecf76
Mike Hartley <mikehartley@users.noreply.github.com>
2014-09-24 14:02:05 +0100

    Converted example to integration test.

--- src/test/java/TryOutIT.java
+++ src/test/java/TryOutIT.java
@@ -1,3 +1,6 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -5,17 +8,31 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class TryOut  {
-    public static void main(String[] args) {
-        // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface, 
-        // not the implementation.
-        WebDriver driver = new FirefoxDriver();
+import static org.junit.Assert.assertEquals;
 
+public class TryOutIT {
+
+    private WebDriver driver;
+
+    @Before
+    public void beforeEveryTest() {
+        // Create a new instance of the Firefox driver
+        // Notice that the remainder of the code relies on the interface,
+        // not the implementation.
+        driver = new FirefoxDriver();
+    }
+
+    @After
+    public void afterEveryTest() {
+        //Close the browser
+        driver.quit();
+    }
+
+    @Test
+    public void firstTryoutIT() {
         // And now use this to visit Google
-        driver.get("http://www.google.com");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
+//        driver.get("http://www.google.co.uk"); // an alternative way
+        driver.navigate().to("http://www.google.co.uk");
 
         // Find the text input element by its name
         WebElement element = driver.findElement(By.name("q"));
@@ -38,9 +55,6 @@ public class TryOut  {
         });
 
         // Should see: "cheese! - Google Search"
-        System.out.println("Page title is: " + driver.getTitle());
-
-        //Close the browser
-        driver.quit();
+        assertEquals("Cheese! - Google Search", driver.getTitle());
     }
 }
